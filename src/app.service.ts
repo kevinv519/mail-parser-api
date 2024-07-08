662ba78eb8cb9b6b3537b1b861671f0a5c0b53e6
Kevin Velasquez <kevin.vela519@gmail.com>
2024-07-08 16:31:50 -0600

    feat: add scraper for email nested links

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUptime(): string {
+    return this.formatUptime(process.uptime());
+  }
+
+  formatUptime(uptime: number): string {
+    const hours = Math.floor(uptime / (60 * 60))
+      .toString()
+      .padStart(2, '0');
+    const minutes = Math.floor((uptime % (60 * 60)) / 60)
+      .toString()
+      .padStart(2, '0');
+    const seconds = Math.floor(uptime % 60)
+      .toString()
+      .padStart(2, '0');
+    return `Server has been running for ${hours}:${minutes}:${seconds} (hh:mm:ss)`;
   }
 }
--- src/email-parser/processors/html-content.processor.ts
+++ src/email-parser/processors/html-content.processor.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { JSDOM } from 'jsdom';
 import { ParsedMail } from 'mailparser';
 import { JsonProcessor } from './processor.interface';
@@ -8,36 +8,77 @@ import { isURL } from 'class-validator';
 
 @Injectable()
 export class HtmlContentProcessor implements JsonProcessor {
+  private readonly logger = new Logger(HtmlContentProcessor.name);
+
   constructor(private readonly httpService: HttpService) {}
 
   async process(parsedMail: ParsedMail): Promise<Record<string, unknown>> {
-    const links = this.getMailLinks(parsedMail.textAsHtml);
+    const links = this.getLinksFromHtmlText(parsedMail.textAsHtml);
 
-    for (const link of links) {
-      const response = await lastValueFrom(this.httpService.get(link));
-      const contentType: string = response.headers['content-type'];
+    const emailLinkResponses = await Promise.allSettled(this.fetchResponsesFromLinks(links));
 
-      if (contentType.includes('application/json;')) {
-        return response.data;
+    const nestedLinks: string[] = [];
+
+    for (const response of emailLinkResponses) {
+      if (response.status === 'fulfilled') {
+        if (response.value.contentType.includes('application/json;')) {
+          return response.value.content;
+        } else if (response.value.contentType.includes('text/html;')) {
+          nestedLinks.push(...this.getLinksFromHtmlText(response.value.content, response.value.link));
+        }
       }
     }
+
+    this.logger.debug('Original email content did not contain a link to a JSON resource');
+
+    const nestedLinksResponses = await Promise.allSettled(this.fetchResponsesFromLinks(nestedLinks));
+
+    for (const response of nestedLinksResponses) {
+      if (response.status === 'fulfilled' && response.value.contentType.includes('application/json;')) {
+        return response.value.content;
+      }
+    }
+
+    this.logger.debug('Nested content from original email links did not contain a link to a JSON response either.');
 
     throw new Error('Email did not have a reference to a JSON link');
   }
 
-  getMailLinks(htmlText: string): string[] {
+  /**
+   * Parses HTML content from a string, identifies anchor tags within the elements and returns and array with valid URL values.
+   * @param htmlText text to parse to an HTML DOM.
+   * @param origin If provided, this value will be prepended to the anchor href value if the reference starts with "/".
+   *
+   * If not provided, href values that start with "/"" will be omitted from the resulting array
+   * @returns array of all links found within the HTML content
+   */
+  private getLinksFromHtmlText(htmlText: string, origin?: string): string[] {
     const {
       window: { document: mailDocument },
     } = new JSDOM(htmlText);
 
     const links: string[] = [];
 
-    for (const aElement of mailDocument.querySelectorAll('a')) {
-      if (aElement.href && isURL(aElement.href, { require_protocol: true, protocols: ['https', 'http'] })) {
-        links.push(aElement.href);
+    for (const anchor of mailDocument.querySelectorAll('a')) {
+      const link = prependOriginToLinkIfNeeded(anchor, origin);
+      if (isURL(link, { require_protocol: true, protocols: ['https', 'http'] })) {
+        links.push(link);
       }
     }
 
     return links;
   }
+
+  private fetchResponsesFromLinks(links: string[]): Promise<{ contentType: string; content: any; link: string }>[] {
+    return links.map(async (link) => {
+      const response = await lastValueFrom(this.httpService.get(link));
+      const contentType: string = response.headers['content-type'];
+
+      return { contentType, content: response.data, link };
+    });
+  }
+}
+
+function prependOriginToLinkIfNeeded({ href }: HTMLAnchorElement, origin?: string) {
+  return origin && href.startsWith('/') ? `${origin}${href}` : href;
 }
